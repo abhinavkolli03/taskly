9a7e00a92a7b8cc91e7f218a458114e08278d536
Abhinav Kolli <abhinavkolli03@gmail.com>
2022-06-30 12:48:29 -0500

    started components and finished helper reducer functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import storage from './storing/storage';
+import '../node_modules/bulma/css/bulma.min.css';
+import '../node_modules/fortawesome/fontawesome-free/css/all.min.css'
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={storage}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
